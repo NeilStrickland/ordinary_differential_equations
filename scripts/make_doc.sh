ebb1bf3f92ee19fdb93ed4ba77a45ceda4ec31aa
Neil Strickland <N.P.Strickland@sheffield.ac.uk>
2020-02-09 16:52:45 +0000

    Add scripts folder

---
+++ scripts/make_doc.sh
@@ -0,0 +1,6 @@
+cp ../lectures/all_lectures.pdf ../docs/lectures
+cp ../lectures/all_handouts.pdf ../docs/lectures
+cp ../probs/all_probs.pdf ../docs/probs
+cp ../exams/all_questions.pdf ../docs/exams
+cp -r ../web ../docs
+
